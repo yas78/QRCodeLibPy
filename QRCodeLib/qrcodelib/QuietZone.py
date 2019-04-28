4ad1240f33314349f15aebd7332629ac7ef7a540
Yasunori Ikeda <aug.nydi@gmail.com>
2019-04-28 19:34:34 +0900

    リファクタリング

--- QRCodeLib/qrcodelib/QuietZone.py
+++ QRCodeLib/qrcodelib/QuietZone.py
@@ -6,22 +6,18 @@ from .misc.ArrayUtil import ArrayUtil
 class QuietZone(object):
     """
         クワイエットゾーン
-    """    
-    _QUIET_ZONE_WIDTH = 4
+    """
+    _WIDTH = 4
 
     @classmethod
     def place(cls, module_matrix: List[List[int]]) -> List[List[int]]:
         """
             クワイエットゾーンを追加します。
         """
-        num_modules_one_side = len(module_matrix) + cls._QUIET_ZONE_WIDTH * 2
+        num_modules_one_side = len(module_matrix) + cls._WIDTH * 2
         ret = [[0] * num_modules_one_side for row in range(num_modules_one_side)]
 
-        for i, row in enumerate(module_matrix):   
-            ArrayUtil.copy(row, 
-                           0, 
-                           ret[i + cls._QUIET_ZONE_WIDTH], 
-                           cls._QUIET_ZONE_WIDTH, 
-                           len(row))
+        for i, row in enumerate(module_matrix):
+            ArrayUtil.copy(row, 0, ret[i + cls._WIDTH], cls._WIDTH, len(row))
 
         return ret
--- QRCodeLib/qrcodelib/misc/ArrayUtil.py
+++ QRCodeLib/qrcodelib/misc/ArrayUtil.py
@@ -4,17 +4,13 @@ from typing import Sequence, MutableSequence
 class ArrayUtil(object):
 
     @classmethod
-    def copy(cls, 
-             source_array: Sequence,
-             source_index: int,
-             destination_array: MutableSequence,
-             destination_index: int,
+    def copy(cls,
+             src_array: Sequence,
+             src_index: int,
+             dest_array: MutableSequence,
+             dest_index: int,
              length: int):
         """
             一次元配列をコピーします。
         """
-        dst_index = destination_index
-
-        for src_index in range(source_index, source_index + length):
-            destination_array[dst_index] = source_array[src_index]
-            dst_index += 1
+        dest_array[dest_index:dest_index + length] = src_array[src_index:src_index + length]
