30d16a92cbdd6850a11fc1568284c112029c4e6a
yas78 <aug.nydi@gmail.com>
2019-12-26 22:14:15 +0900

    不要ファイルを削除

--- QRCodeLib/Test_qrcodelib.py
+++
@@ -1,5 +0,0 @@
-import qrcodelib as qr
-
-symbols = qr.Symbols()
-symbols.append_text("abcdefghijklmn")
-symbols.item(0).save_bitmap(r"C:\Users\owner\Documents\Visual Studio 2019\Projects\github\QRCodeLibPy\test.bmp")
